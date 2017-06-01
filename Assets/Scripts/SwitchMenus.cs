63584c3081bf0a2b1e85daf49407a98e743a41f9
matthewguillory65 <matthewguillory65@gmail.com>
2017-06-01 16:40:48 -0500

    Fixing Some buttons, fixed the Options to Pause Menu button

--- Assets/Scripts/SwitchMenus.cs
+++ Assets/Scripts/SwitchMenus.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,6 +8,8 @@ public class SwitchMenus: MonoBehaviour
     public Canvas mainCanvas;
     public Canvas optionsCanvas;
     public Canvas controlsCanvas;
+    public Canvas pauseCanvas;
+    public Canvas pause_OptionCanvas;
 
 
     public void CanvastoOptions()
@@ -15,10 +18,32 @@ public class SwitchMenus: MonoBehaviour
         optionsCanvas.enabled = true;
     }
 
-    public void CanvastoMain()
+    public void PausetoOptions()
+    {
+        pauseCanvas.enabled = false;
+        pause_OptionCanvas.enabled = true;
+    }
+
+    public void OptionstoPause()
+    {
+        pause_OptionCanvas.enabled = false;
+        pauseCanvas.enabled = true;
+    }
+
+    public void PausetoGame()
+    {
+        pauseCanvas.enabled = false;
+    }
+
+    public void OptionstoMain()
     {
         mainCanvas.enabled = true;
         optionsCanvas.enabled = false;
+    }
+
+    public void ControlstoMain()
+    {
+        mainCanvas.enabled = true;
         controlsCanvas.enabled = false;
     }
 
@@ -28,8 +53,16 @@ public class SwitchMenus: MonoBehaviour
         controlsCanvas.enabled = true;
     }
 
+    public void PauseMenutoMainMenu()
+    {
+        //Application.LoadLevel("MainMenu");
+        mainCanvas.enabled = true;
+        pauseCanvas.enabled = false;
+    }
+
     public void PlayGame()
     {
-        Application.LoadLevel("scene");
+        //Application.LoadLevel("ActualGame");
+        mainCanvas.enabled = false;
     }
 }
