fe1f90c186d7d996437a43bd2137270c413dc8b3
matthewguillory65 <matthewguillory65@gmail.com>
2017-06-01 15:37:29 -0500

    Menu
    This is the working menu, with it switching canvas and ready to start
    the game

---
+++ Assets/Scripts/ExitApplication.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitApplication : MonoBehaviour
+{
+    public void QuitApplication()
+    {
+        Application.Quit();
+        Debug.Log("I'm working");
+    }
+}
---
+++ Assets/Scripts/SwitchMenus.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchMenus: MonoBehaviour
+{
+    public Canvas mainCanvas;
+    public Canvas optionsCanvas;
+    public Canvas controlsCanvas;
+
+
+    public void CanvastoOptions()
+    {
+        mainCanvas.enabled = false;
+        optionsCanvas.enabled = true;
+    }
+
+    public void CanvastoMain()
+    {
+        mainCanvas.enabled = true;
+        optionsCanvas.enabled = false;
+        controlsCanvas.enabled = false;
+    }
+
+    public void CanvastoControls()
+    {
+        mainCanvas.enabled = false;
+        controlsCanvas.enabled = true;
+    }
+
+    public void PlayGame()
+    {
+        Application.LoadLevel("scene");
+    }
+}
