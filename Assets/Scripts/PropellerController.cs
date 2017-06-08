9dba6f9f88bc983f50a159c7731cb3f13529d1f9
ZyronMelancon <altausolaris@gmail.com>
2017-06-08 17:58:05 -0500

    edits

--- Assets/Scripts/PropellerController.cs
+++ Assets/Scripts/PropellerController.cs
@@ -6,27 +6,20 @@ public class PropellerController : MonoBehaviour {
 
     public GameObject LProp1;
     public GameObject RProp1;
-    public float IdleSpinRate;
     public float RunningSpinRate;
+
+    private Rigidbody body;
 
 	// Use this for initialization
 	void Start ()
     {
-	    
+        body = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetAxis("Vertical") == 0)
-        {
-            LProp1.transform.Rotate(0, 0, -IdleSpinRate);
-            RProp1.transform.Rotate(0, 0, IdleSpinRate);
-        }
-        else
-        {
-            LProp1.transform.Rotate(0, 0, -RunningSpinRate);
-            RProp1.transform.Rotate(0, 0, RunningSpinRate);
-        }
+            LProp1.transform.Rotate(0, 0, -RunningSpinRate + -body.velocity.magnitude);
+            RProp1.transform.Rotate(0, 0, RunningSpinRate + body.velocity.magnitude);
 	}
 }
