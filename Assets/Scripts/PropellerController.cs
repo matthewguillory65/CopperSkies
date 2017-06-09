9d970ab449244b27d05d0119872af9d200b92989
ZyronMelancon <altausolaris@gmail.com>
2017-06-09 12:07:39 -0500

    added rigged model

--- Assets/Scripts/PropellerController.cs
+++ Assets/Scripts/PropellerController.cs
@@ -6,20 +6,46 @@ public class PropellerController : MonoBehaviour {
 
     public GameObject LProp1;
     public GameObject RProp1;
+    public GameObject LProp2;
+    public GameObject RProp2;
     public float RunningSpinRate;
 
     private Rigidbody body;
+
+    private PlayerMovement shipstuff;
 
 	// Use this for initialization
 	void Start ()
     {
         body = GetComponent<Rigidbody>();
+        shipstuff = GetComponent<PlayerMovement>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-            LProp1.transform.Rotate(0, 0, -RunningSpinRate + -body.velocity.magnitude);
-            RProp1.transform.Rotate(0, 0, RunningSpinRate + body.velocity.magnitude);
-	}
+        LProp1.transform.Rotate(0, 0, (-RunningSpinRate + -body.velocity.magnitude) / 2);
+        RProp1.transform.Rotate(0, 0, (RunningSpinRate + body.velocity.magnitude) / 2);
+        LProp2.transform.Rotate(0, 0, (RunningSpinRate + body.velocity.magnitude) / 2);
+        RProp2.transform.Rotate(0, 0, (-RunningSpinRate + -body.velocity.magnitude) / 2);
+    }
+
+    public void UpdateUpgrade()
+    {
+        switch(shipstuff.UpgradeNumber)
+        {
+            case 1:
+                RProp2.SetActive(false);
+                LProp2.SetActive(false);
+                break;
+            case 2:
+                RProp2.SetActive(true);
+                LProp2.SetActive(true);
+                break;
+            case 3:
+                break;
+            default:
+                break;
+        }
+    }
 }
