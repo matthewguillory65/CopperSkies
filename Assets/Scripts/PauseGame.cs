bcab58eff34c2ee94fb966eea64983152d3cb83a
matthewguillory65 <matthewguillory65@gmail.com>
2017-06-15 11:38:12 -0500

    CannonBallDelete file fixed an error, linking all the canvas together through the scenes

--- Assets/Scripts/PauseGame.cs
+++ Assets/Scripts/PauseGame.cs
@@ -1,19 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseGame : MonoBehaviour
 {
 
     public Canvas PauseCanvas;
-    
+    public Canvas OptionsCanvas;
+
+    public void Start()
+    {
+        Debug.Log("Started");
+    }
 	
 	// Update is called once per frame
-	void Update ()
+	public void Update()
+	{
+        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+            Debug.Log("Im working");
+        }
+    }
+
+    public void PauseToOptions()
     {
-        if (Input.GetKeyDown(KeyCode.Escape) && PauseCanvas.enabled == false)
-            PauseCanvas.enabled = true;
-        if (Input.GetKeyDown(KeyCode.Escape) && PauseCanvas.enabled == true)
-            PauseCanvas.enabled = false;
+        PauseCanvas.enabled = false;
+        OptionsCanvas.enabled = true;
+    }
+
+    public void OptionsToPause()
+    {
+        OptionsCanvas.enabled = false;
+        PauseCanvas.enabled = true;
+    }
+
+    public void ResumeGame()
+    {
+        PauseCanvas.enabled = false;
+    }
+
+    public void ToMainMenu()
+    {
+        Application.LoadLevel("Scene.1_Menus");
+    }
+
+    public void Pause()
+    {
+        PauseCanvas.enabled = true;
     }
 }
