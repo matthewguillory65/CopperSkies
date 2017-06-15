191e254bd8dc4927b0c35e4dcb41ce0131e3708f
ZyronMelancon <altausolaris@gmail.com>
2017-06-15 13:05:23 -0500

    cannon script

--- Assets/Scripts/CannonsBehaviour.cs
+++ Assets/Scripts/CannonsBehaviour.cs
@@ -12,21 +12,47 @@ public class CannonsBehaviour : MonoBehaviour {
     public float MaxX;
     public float MaxY;
 
+    Cannon L;
+    Cannon R;
+    Cannon Selected;
+    class Cannon
+    {
+        public GameObject Base;
+        public GameObject Main;
+
+        public Cannon(GameObject B, GameObject M)
+        {
+            Base = B;
+            Main = M;
+        }
+    }
 
 	// Use this for initialization
 	void Start ()
     {
-		
+        L = new Cannon(CannonBaseL, CannonMainL);
+        R = new Cannon(CannonBaseR, CannonMainR);
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
-	}
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+            Selected = L;
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+            Selected = R;
+
+        if (Selected != null)
+        {
+            CannonLook();
+            if (Input.GetKeyDown(KeyCode.Mouse1) || Input.GetKeyDown(KeyCode.Mouse0))
+                Selected = null;
+        }
+    }
 
     void CannonLook()
     {
-
+        Selected.Base.transform.Rotate(0, Input.GetAxis("Mouse X"), 0);
+        Selected.Main.transform.Rotate(Input.GetAxis("Mouse Y"), 0, 0);
     }
 }
