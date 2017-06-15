243e5042f43411181b6a320ce742e4df89eafa9e
matthewguillory65 <matthewguillory65@gmail.com>
2017-06-15 15:33:25 -0500

    Fixes to the cannonball and how long it takes to shoot

--- Assets/Scripts/CannonBallShoot.cs
+++ Assets/Scripts/CannonBallShoot.cs
@@ -1,21 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using NUnit.Framework.Constraints;
 using UnityEngine;
 
 public class CannonBallShoot : MonoBehaviour
 {
     public GameObject CannonBallPrefab;
-    public float speed = 20;
+    public float force = 2500;
     public float timeTillShootAgain = 5;
 
+    public void Start()
+    {
+        StartCoroutine(Shoot());
+    }
     public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        
+    }
+
+    IEnumerator Shoot()
+    {
+        while (true)
         {
-            GameObject cannonball = Instantiate(CannonBallPrefab, transform);
-            cannonball.transform.parent = null;
-            cannonball.GetComponent<Rigidbody>().AddRelativeForce(2700, 0, 0);
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                GameObject cannonball = Instantiate(CannonBallPrefab, transform);
+                cannonball.transform.parent = null;
+
+                cannonball.GetComponent<Rigidbody>().AddRelativeForce(force, 0, 0);
+                yield return new WaitForSeconds(timeTillShootAgain);
+            }
+            yield return new WaitForSeconds(0.01f);
         }
     }
+
 }
-//yield return new WaitForSeconds(timeTillShootAgain);
